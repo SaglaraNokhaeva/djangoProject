75182bb79b9ca9ed07e7fe83f787fcece3dcfd03
Saglara <nohaevass@yandex.ru>
2023-10-16 23:30:55 +0300

    1

--- myapp/management/commands/create_order.py
+++ myapp/management/commands/create_order.py
@@ -6,16 +6,17 @@ class Command(BaseCommand):
     help = "Create order."
 
     def add_arguments(self, parser):
-        parser.add_argument('id', type=int, help='Сustomer ID')
+        parser.add_argument('id_customer', type=int, help='Сustomer ID')
+        parser.add_argument('id_product', type=int, help='product ID')
     def handle(self, *args, **kwargs):
-        # customer = models.ForeignKey(Сustomer, on_delete=models.CASCADE)
-        # products = models.ManyToManyField(Product)
-        # amount = models.DecimalField(max_digits=8, decimal_places=2)
-        # date = models.DateTimeField(auto_now_add=True)
 
         # customer = Сustomer(name='John', email='john@example.com',
         # phone='9999999999', adr='hdfjs hfjsf hfjkksdhf', date = '2025-09-09')
-        order = Order(customer=customer, products='Name1',
+        id_customer = kwargs['id_customer']
+        id_product = kwargs['id_product']
+        customer = Сustomer.objects.get(id=id_customer)
+        products = Сustomer.objects.get(id=id_product)
+        order = Order(customer=customer, products=products,
                             amount='123456.58', date='2023-09-09')
         order.save()
         self.stdout.write(f'{order}')
