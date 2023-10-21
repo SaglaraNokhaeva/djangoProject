80f10c045d37eee995299bc518f36bdada14f2ff
Saglara <nohaevass@yandex.ru>
2023-10-21 19:52:12 +0300

    1

--- myapp/management/commands/create_order.py
+++ myapp/management/commands/create_order.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from myapp.models import Order, Сustomer
+from myapp.models import Order, Сustomer, Product
 
 
 class Command(BaseCommand):
@@ -7,16 +7,24 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('id_customer', type=int, help='Сustomer ID')
-        parser.add_argument('id_product', type=int, help='product ID')
+        parser.add_argument('-p', '--Product_id', nargs='+', help="customer ID", required=True)
     def handle(self, *args, **kwargs):
+        id_customer = kwargs.get('id_customer')
+        Product_id: list = kwargs.get('Product_id')
+        customer = Сustomer.objects.filter(pk=id_customer).first()
 
-        # customer = Сustomer(name='John', email='john@example.com',
-        # phone='9999999999', adr='hdfjs hfjsf hfjkksdhf', date = '2025-09-09')
-        id_customer = kwargs['id_customer']
-        id_product = kwargs['id_product']
-        customer = Сustomer.objects.get(id=id_customer)
-        products = Сustomer.objects.get(id=id_product)
-        order = Order(customer=customer, products=products,
-                            amount='123456.58', date='2023-09-09')
-        order.save()
-        self.stdout.write(f'{order}')
+        order = Order(customer=customer)
+        total_price = 0
+        for i in range(0, len(Product_id)):
+            product = Product.objects.filter(pk=Product_id[i]).first()
+            total_price += float(product.price)
+            order.amount = total_price
+            order.save()
+            order.products.add(product)
+
+        # customer = Сustomer.objects.get(id=id_customer)
+        # products = Сustomer.objects.get(id=id_product)
+        # order = Order(customer=customer, products=products,
+        #                     amount='123456.58', date='2023-09-09')
+        # order.save()
+        # self.stdout.write(f'{order}')
